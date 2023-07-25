f01aff7e11cf5c0c7cbeb5466f4997fdc286d8b1
Yan Gutberg <guttlauncher@gmail.com>
2023-07-25 12:42:02 -0300

    Conectado com o banco de dados Mongoose

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-// initial config
+// configuracao inicial
+require('dotenv').config();
+
+const e = require('express');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-// way to read JSON / using middlewares
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD); // esse 'encodeURIComponent' corrige um possivel erro de unescaped caracters
+
+// forma para ler JSON / usando middlewares
 
 app.use(
     express.urlencoded({
@@ -12,11 +19,11 @@ app.use(
 
 app.use(express.json());
 
-// intial root / endpoint
+// rota inicial / endpoint
 
 app.get('/' , (req, res) => {
 
-    //show req
+    //mostrar req
 
     res.json({
         message: "OI Express!",
@@ -24,5 +31,12 @@ app.get('/' , (req, res) => {
 
 });
 
-// give a port
-app.listen(3000);
+// dar uma porta
+mongoose.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.mbjqfcs.mongodb.net/?retryWrites=true&w=majority`,
+    ).then(() => {
+        app.listen(3000);
+        console.log("Conectado com sucesso!");
+    }).catch((e) => {
+        console.log(e);
+    })
