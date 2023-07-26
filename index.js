7876ef5cef735708706333f46c8f599fca850134
Yan Gutberg <guttlauncher@gmail.com>
2023-07-26 15:11:30 -0300

    Inserindo o objeto Pessoa no BD

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // configuracao inicial
 require('dotenv').config();
 
-const e = require('express');
-const express = require('express');
-const mongoose = require('mongoose');
+const e = require("express");
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD); // esse 'encodeURIComponent' corrige um possivel erro de unescaped caracters
+
+const Person = require("./models/Person");
 
 // forma para ler JSON / usando middlewares
 
@@ -19,16 +21,49 @@ app.use(
 
 app.use(express.json());
 
+// rotas da API
+
+app.post("/person", async (req, res) => {
+    
+    // request do body    
+    const {name, age, salary, aproved} = req.body; // Criar variaveis assim é um recurso do js, chamado Destructuring
+
+    if(!name) {
+        res.status(422).json({error: "O nome é obrigatório!"});
+    } else if (!age) {
+        res.status(422).json({error: "A idade é obrigatória!"});
+    }
+
+    const person = {
+        name,
+        age,
+        salary,
+        aproved
+    };
+
+    // metodo 'create' do mongoose
+    try {
+        //criando dados
+        await Person.create(person);
+
+        res.status(201).json({ // o status http 201 significa que algum recurso foi criado com sucesso no servidor
+            message: "Pessoa inserida no sistema com sucesso!"
+        }); 
+
+    } catch (e) {
+        res.status(500).json({ // o status http 500 significa Erro Interno no Servidor
+            error: e
+        }); 
+    }
+});
+
 // rota inicial / endpoint
 
 app.get('/' , (req, res) => {
-
     //mostrar req
-
     res.json({
         message: "OI Express!",
-    })
-
+    }) 
 });
 
 // dar uma porta
@@ -36,7 +71,7 @@ mongoose.connect(
     `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.mbjqfcs.mongodb.net/?retryWrites=true&w=majority`,
     ).then(() => {
         app.listen(3000);
-        console.log("Conectado com sucesso!");
+        console.log("Conectado com sucesso ao MongoDB!");
     }).catch((e) => {
         console.log(e);
-    })
+    });
--- models/Person.js
+++ models/Person.js
@@ -1,1 +1,10 @@
 const mongoose = require('mongoose');
+
+const Person = mongoose.model('Person', {
+    name: String,
+    age: Number,
+    salary: Number,
+    aproved: Boolean,
+});
+
+module.exports = Person;
